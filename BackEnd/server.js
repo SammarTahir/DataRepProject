294df6b2817f4a0fb217951c2b72ba6051cc8428
SammarTahir <G00347526@gmit.ie>
2018-11-08 20:17:42 +0000

    basic setup

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -20,6 +20,7 @@ app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept");
     next();
@@ -44,6 +45,9 @@ app.post('/api/posts', function (req, res) {
         title: req.body.title,
         content: req.body.content
     })
+
+    // adding this text will close server (stopping double posts)
+    res.send('Item added');
 })
 
 app.get('/api/posts', function (req, res) {
@@ -56,6 +60,14 @@ app.get('/api/posts', function (req, res) {
 
 })
 
+app.delete('/api/posts/:id', function (req, res) {
+    console.log(req.params.id);
+
+    PostModel.deleteOne({ _id: req.params.id },
+        function (err, data) {
+            res.send(data);
+        })
+})
 
 var server = app.listen(8081, function () {
     var host = server.address().address
